c7fd12a84294b00d609e937485eb8250747cf081
Kopperband <nathan.hansford@me.com>
2024-06-26 21:18:23 -0300

    bb

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-@secret = ENV['SECRET_KEY_BASE'] || 'e6f049159561e1e1'
+@secret = ENV['SECRET_KEY_BASE'] || 'e6f049159561e1e'
